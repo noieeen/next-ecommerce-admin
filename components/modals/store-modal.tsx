40990811a733aa13c5e0230f4b4a8fefd1833fa3
noieeen <tharnnn1401@gmail.com>
2023-08-27 03:17:04 +0700

    navbar and stores switcher
    https://youtu.be/5miHyP6lExg

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -17,7 +17,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { toast } from "react-hot-toast";
 
 const formSchema = z.object({
